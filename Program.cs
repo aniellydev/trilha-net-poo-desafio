606bd97c0a3c69a2f13ba4e4e5cacd73dd773912
aniellydev <151768183+aniellydev@users.noreply.github.com>
2023-12-16 16:07:34 -0300

    feat: adicionando solução no program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia: ");
+        Nokia nokia = new Nokia("987455200", "Modelo 1", "123456", 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone: ");
+        Iphone iphone = new Iphone("123456", "Iphone 11", "654321", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+    }
+}
